e9b428cb05a3cd5d2de02827a6030207c74d80f5
Michael Lu <michael07161990@gmail.com>
2020-01-05 13:56:16 -0500

    added paypal button env process file annd gitignore file updated

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -31,7 +31,7 @@ export default class MyApp extends React.Component {
     // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
     const client = {
-      sandbox: "Aep8yw4NNbUXvgXG2BOwk7FNj8igTcR9N7E6pILJNZXu-X3dTSe-dChCKTrGZIEnUBY3zBvD9KQYdFyD",
+      sandbox: process.env.REACT_APP_APP_ID,
       production: "YOUR-PRODUCTION-APP-ID"
     };
     // In order to get production's app-ID, you will have to send your app to Paypal for approval first
